eb9fc8395f76a4484f7b7df51164724090face55
xiaokamikami <fengkehan@bosc.ac.cn>
2024-06-12 18:36:52 +0800

    main: add pldm_command

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 #include <time.h>
 #include <boost/process.h>
 #include <iostream.h>
+#include <cstring>
 #include <vector>
 
 #include "directed_tbs.h"
@@ -18,14 +19,21 @@ int q2d_fifo;
 double CPI[NumCores] = {};
 
 namespace bp = boost::process;
+char *workload_name = "./wokrload.gz";
+char *mem_name = "./wokrload.dat";
+char *gcpt_name = "./gcpt.bin";
 
+std::string get_pldm_command(char *gpct, char *workload, uint64_t max_ins);
+std::string get_bin2addr_command(char *gpct, char *workload);
 int main(int argc, char *argv[])
 {
     const char *detail_to_qemu_fifo_name = "./detail_to_qemu.fifo";
     const char *qemu_to_detail_fifo_name = "./qemu_to_detail.fifo";
     const char *emu_to_cpi_txt_name = "./emu_to_cpi_file.txt"
 
-    std::string pldm_command = "make pldm-run";
+    std::string pldm_command = get_pldm_command(gcpt_name, workload_name, 40000000);
+    std::string bin2addr_commmand = get_bin2addr_command(gcpt_name, workload_name);
+
     // creact pipline
     bp::ipstream pipe_stream;
 
@@ -69,4 +77,25 @@ int main(int argc, char *argv[])
     write(d2q_fifo, &d2q_buf, sizeof(Detail2Qemu));
 
     return 0;
+}
+
+
+std::string get_pldm_command(char *gpct, char *workload, uint64_t max_ins) {
+    std::string base_command = "make pldm-run";
+    std::string base_arggs = "PLDM_EXTRA_ARGS=""+=diff=./XiangShan/ready-to-run/riscv64-nemu-interpreter-dual-so";
+
+    char args[512];
+    sprintf(args, "+wokrload=%s +gcpt-restore=%s +max-instrs=%ld""" , workload, gcpt);
+
+    std::strcat(base_command, base_arggs);
+    base_command.append(args);
+
+    return base_command;
+}
+
+std::string get_bin2addr_command(char *gpct, char *workload) {
+//exmaple ./bin2ddr -i $ckpt -o ./out.dat -m "row,ba,col,bg" -r $gcpt
+
+
+
 }
