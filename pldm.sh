c3ba2e3ce481995aed03a5ee1899c98ecfe23149
xiaokamikami <fengkehan@bosc.ac.cn>
2024-06-30 11:23:54 +0800

    fix pdlm sh

--- pldm.sh
+++ pldm.sh
@@ -5,5 +5,5 @@ gcpt_path=$(pwd)/gcpt.bin
 nemu=$(pwd)/riscv64-nemu-interpreter-dual-so
 cd ./XiangShan
 source ~/TOOLS/env.sh
-make pldm-run PLDM_EXTRA_ARGS="+=diff=$nemu +wokrload=$checkpoint_path +gcpt-restore=$gcpt_path +max-instrs=4000000"
+make pldm-run PLDM_EXTRA_ARGS="+diff=$nemu +workload=$checkpoint_path +gcpt-restore=$gcpt_path +max-instrs=4000000 +overwrite_nbytes=64107252"
 cd ..
